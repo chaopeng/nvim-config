babd62451880ea418c91788e0c14d98c92c6831c
chaopeng <chaopeng@chaopeng.me>
2023-12-24 22:13:25 -0500

    keybinding: cc for copy and cv for paste

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,5 +6,11 @@ local map = vim.keymap.set
 
 map("n", "<leader>wt", "<cmd> split term://fish<cr>", { desc = "Open a terminal in hsplit window" })
 
--- also support t to yank. for mouse friendly.
-map("v", "t", "y")
+-- also support cc to yank. for mouse friendly.
+map("v", "cc", "y")
+-- in normal mode, just copy whole line.
+map("n", "cc", "yy")
+
+-- cv for paste
+map("v", "cv", "p")
+map("n", "cv", "0p")
